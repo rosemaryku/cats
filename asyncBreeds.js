37d6f86af8ad4a4da51bb7ab8e3f9395c0e88f47
Rosemary Ku <rosemary.ku@hotmail.com>
2022-01-18 14:01:42 -0500

    Add tests

---
+++ asyncBreeds.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const breedDetails = require("./syncBreeds");
+
+const breedDetailsFromFile = function (breed, secondFunc) {
+  console.log("breedDetailsFromFile: Calling readFile...");
+
+  fs.readFile(`./data/${breed}.txt`, "utf8", (error, data) => {
+    // CHANGE: Pass data into callback instead of returning it directly
+    console.log("In readFile's Callback: it has the data.");
+    // if (!error) secondFunc(data);
+    secondFunc(data);
+  });
+};
+// CHANGE 1: Moved the console.log into a new function:
+const printOutCatBreed = (breed) => {
+  console.log("Return Value: ", breed); // => print out details correctly.
+};
+
+module.exports = breedDetailsFromFile;
